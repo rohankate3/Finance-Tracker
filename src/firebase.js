cde1b605d460633e799e703b28ba2b55f0867558
[Rohan Kate] <[rohankate1313@gmail.com]>
2024-08-03 03:37:12 +0530

    firebase code changed

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,6 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAqq1Bl1xDDd9wV1Sq-ZdQyei7dxaFiVWw",
-  authDomain: "financly-48898.firebaseapp.com",
-  projectId: "financly-48898",
-  storageBucket: "financly-48898.appspot.com",
-  messagingSenderId: "297396114613",
-  appId: "1:297396114613:web:1f217245755fccd6d11afc",
-  measurementId: "G-Y6V5HLJW88"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
